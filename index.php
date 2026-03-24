3cf088d918b017bcf6eb90f187a99e3f2cdbfc0a
Zhou Junyang <junyang.zhou@gmail.com>
2026-03-24 15:53:19 +0800

    upload index.php
    index.php

---
+++ index.php
@@ -0,0 +1,509 @@
+<HTML>
+<HEAD>
+	<TITLE>Dr. Zhou Junyang's homepage</TITLE>
+	<META NAME="keywords" CONTENT="Zhou Junyang, Junyang Zhou, ZhouJunyang, Zhou Jun Yang, Zhou J. Y., Zhou J., J. Y. Zhou, J. Zhou, JYZhou">
+	<meta http-equiv="Content-Type" content="text/html;charset=big5">
+	<link href="./css/jyzhou.css" rel=stylesheet>
+
+</HEAD>
+	
+<BODY>
+	<CENTER>
+			<table width=800 border=0 cellpadding=0 cellspacing=0>
+				<tr>
+					<td bgcolor=#7CA3DE height=10>
+						<div align="center">							
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td bgcolor=#EEEEEE>
+						<div align="center">
+							<span class="title_1">Hong Kong Baptist University</span>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td bgcolor=#EEEEEE>
+						<div align="center">
+							<span class="title_1">Department of Computer Science</span>
+						</div>
+					</td>
+				</tr>
+				<tr><td bgcolor=#EEEEEE height=10> </td></tr>
+				<tr>
+					<td bgcolor=#7CA3DE height=10>
+						<div align="center">							
+						</div>
+					</td>
+				</tr>				
+			</table>
+			<table width=800 border=0 cellpadding=0 cellspacing=0>
+				<tr>
+					<td height=10></td>
+				</tr>
+				<tr>
+					<td bgcolor=#000090>
+						<div align="center"><span class="menubar">
+						     || <a href="./index.php">Home</a> ||
+						     	<a href="./research.html">Research</a> ||
+							<a href="./teaching.html">TA Course</a> ||
+							<a href="./publication.html">Publications</a> ||
+							<a href="./award.html">Awards</a> ||
+							<a href="./download.html">Source</a> ||
+							<a href="./links.html">Bookmarks</a> ||
+						</span></div>
+					</td>
+				</tr>
+				<tr>
+					<td height=10></td>
+				</tr>
+			</table>
+			
+	
+		<TABLE width=800 border=0 cellpadding=0 cellspacing=0>
+			<tr>
+                        	<td width=150>
+                          		<div align="left">
+                          			<IMG src="./images/jyzhou1.jpg" width="120" height="150"></IMG>
+                          		</div>    
+                       		</td>
+                        
+		     		<td>
+		     			<table width=650 border=0 cellpadding=0 cellspacing=0>
+						
+
+					<tr>
+						    <td>
+							<div align="left">
+								<span class="font-family:Arial,Helvetica,sans-serif; font-size:35pt; font-weight:bold; color:#000000">
+									<b>Dr. Zhou Junyang</b>
+								</span>
+							</div>
+						    </td>					
+						</tr>
+					
+					<!--<tr><td height="10"></td></tr>-->
+					
+						<tr>     
+							<td>
+				     				<div align="left"> 
+				     					<IMG src="./images/chi_name.gif" width="96" height="32"></IMG>
+								</div>					
+							</td>
+						</tr>
+					
+					<!--	<tr><td height="12"></td></tr>-->
+					
+						<tr>     
+							<td>
+				     				<div align="left">
+				     					<IMG src="./images/email.gif" width="245" height="25"></IMG>
+								</div>					
+							</td>
+						</tr>	
+			             
+			               		<tr>
+			                     		<td >
+			                           		<div align="left">
+		        	                   			<a href="./download/cv.pdf"><span class="url_1"><U>Curriculum Vitae</U></span></a>
+		        	                   			<!--<a href="./download/cv4chinese.pdf"><span class="url_1"><U>Chinese CV</U></span></a>-->
+		                	           		</div>
+		                     			</td>
+		                     	      
+			                     	</tr>	
+					
+			                <!--	<tr>
+			                     		<td >
+			                           		<div align="left">
+		        	                   			<a href="./download/cv.pdf"><span class="url_1"><U>Curriculum Vitae</U></span></a>
+		        	                   			<a href="./download/cv4chinese.pdf"><span class="url_1"><U>Chinese CV</U></span></a>
+		                	           		</div>
+		                     			</td>
+		                     	      
+			                     	</tr>	
+					-->
+
+
+					<!--	<tr><td height="10"></td></tr>-->
+
+					<!-- Insert degree information -->
+		        			<tr>
+		               				<table width=650 border=0 cellpadding=0 cellspacing=0> 
+		                    				<tr>
+		                     					<td >
+		                           					<div align="left">
+		                           						<span class="content_2">Ph.D (2006),</span>
+		                           							<a href="http://www.comp.hkbu.edu.hk" target=_blank><span class="url_1">Department of Computer Science,</span></a>
+		                           							<a href="http://www.hkbu.edu.hk" target=_blank><span class="url_1">HKBU</span></a>
+		                           					</div>
+		                     					</td>
+		                     
+		                				</tr>
+		                       
+		                     				<tr>
+		                     					<td >
+		                           				<div align="left">
+		                           					<span class="content_2"><a href="./award/bsdegree.jpg"><span class="content_2"><U>B. Sc. (2000)</U></span></a>, <a href="./award/msdegree.jpg"><span class="content_2"><U>M. Sc. (2003)</U></span></a>,</span>
+		                           					<a href="http://math.sysu.edu.cn/" target=_blank><span class="url_1">School of Mathematics & Computational Science,</span></a>
+		                           					<a href="http://www.sysu.edu.cn/" target=_blank><span class="url_1">Sun Yat-sen University</span></a>
+		                           				</div>
+		                     				</td>
+		                     	      
+		                     			</tr>
+		                     	
+		                     	<!--<tr><td height="10"><td></tr>-->
+		                        
+		                  			</table>
+		         	
+		         			</tr>
+
+					</table>
+                       		</td>
+	         	</tr>
+		</TABLE>
+		
+
+<!--/////////////Contact information////////////////////-->
+
+		<div align="center">		
+	        	<hr noshade size=2 width="800" color=cccccc>
+	        </div>	
+				
+
+		<table width=800 border=0 cellpadding=0 cellspacing=0> 
+			<tr>
+	   			<td width="200">
+		        		<div align="left">
+		          			<span class="content_1">Contact Information:</span>
+		                	</div>
+	       			</td>
+	        	</tr>			   		
+		</table>   		
+		   		
+		<table width=800 border=0 cellpadding=0 cellspacing=0> 
+		<tr><td width="50" height="10"></td><td></td>
+		<tr>
+		        	<td width="50">
+		         		<div align="left"><span class="content_2">Office:</span></div>
+		        	</td>
+		        
+		          	<td>
+		       			<div align="left">
+		       				<span class="content_2">
+		       				R716, Run Run Shaw Building, Department of Computer Science, Hong Kong Baptist University, Hong Kong
+		       				</span>
+		       			</div>
+	         		</td> 
+	    	        
+	    	</tr>
+		
+		<tr>
+		        	<td width="50">
+		        		<div align="left"><span class="content_2">Telephone:</span></div>		        		
+		        	</td>
+				
+				<td>
+					<div align="left"><span class="content_2">(852)-34115019</span></div>
+				</td>
+		</tr>  
+
+		<tr>
+		        	<td width="50">
+		        		<div align="left"><span class="content_2">Facsimile:</span></div>		        		
+		        	</td>
+				
+				<td>
+					<div align="left"><span class="content_2">(852)-34117892</span></div>
+				</td>
+		        
+		</tr>  
+
+		
+		<!--<tr><td height="20"></td></tr>-->
+		
+				<!--/////////////Mailing Address////////////////////-->
+	
+		<tr>
+		     		<td width="100">
+		        		<div align="left"><span class="content_2">Mailing Address:</span></div>
+		  	        </td>
+
+	     			<td>
+	          			<div align="left"><span class="content_2">R716, Department of Computer Science, Hong Kong Baptist University, KowloonTong ,KL, HK</span></div>
+	  		        </td>
+	        </tr> 	
+		</table>
+	
+	
+	
+		<div align="center">	
+			<hr noshade size=2 width="800" color=cccccc>        
+		</div>	
+		
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+	             	<tr>
+		       		<td >
+		              		<div align="left">
+		          			<span class="content_1">Research Interests:</span>
+		                	</div>
+	        		</td>
+	         	</tr>	
+
+	              	<tr>
+		    		<td >
+		                	<div align="left">
+		                		<span class="content_2">Mobile and Location-aware Computing, Ubiquitous/Pervasive Computing, Real-Time Network and Bayesian Network</span>
+		                        </div>
+		                </td>		                     	
+	                </tr>
+		</table>
+		
+		<div align="center">		
+	        	<hr noshade size=2 width="800" color=cccccc>
+	        </div>	
+
+
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+	             	<tr>
+		       		<td width="200">
+		              		<div align="left">
+		          			<span class="content_1">Professional Membership:</span>
+		                	</div>
+	        		</td>
+	         	</tr>	
+		</table>
+
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">2006---present</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Member of the Association for Computing Machinery (ACM)</span>
+		                        </div>
+		                </td>
+	                </tr>
+	                
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">2006---present</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Member of the Institute of Electrical and Electronic Engineers (IEEE)</span>
+		                        </div>
+		                </td>
+	                </tr>	                
+	                
+		</table>
+
+
+		<div align="center">		
+	        	<hr noshade size=2 width="800" color=cccccc>
+	        </div>	
+
+
+
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+	             	<tr>
+		       		<td width="200">
+		              		<div align="left">
+		          			<span class="content_1">Experience:</span>
+		                	</div>
+	        		</td>
+	         	</tr>	
+		</table>
+
+
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+		<!--
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">01/2007---present</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Postdoctoral Research Fellow, Dept. of CS, HKBU, Hong Kong SAR, China.</span>
+		                        </div>
+		                </td>
+	                </tr>
+		-->	
+
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">09/2006---present</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Research Assistant, Dept. of CS, HKBU, Hong Kong SAR, China.</span>
+		                        </div>
+		                </td>
+	                </tr>
+	                
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">09/2003---09/2006</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Teaching Assistant, Dept. of CS, HKBU, Hong Kong SAR, China.</span>
+		                        </div>
+		                </td>
+	                </tr>	                
+	                
+
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">09/2003---09/2006</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Student, Dept. of CS, HKBU, Hong Kong SAR, China.</span>
+		                        </div>
+		                </td>
+	                </tr>	         
+
+	              	<tr>
+		    		<td width="150">
+		                	<div align="left">
+		                		<span class="content_2">09/1996---09/2003</span>
+		                        </div>
+		                </td>
+
+		    		<td>
+		                	<div align="left">
+		                		<span class="content_2">Student, Dept. of Math, Sun Yat-sen University, Guang Zhou, China.</span>
+		                        </div>
+		                </td>
+	                </tr>	         
+	                
+	                
+		</table>
+
+	        	      
+<!--	        	        
+		<table width=800 border=0 cellpadding=0 cellspacing=0>
+		<tr>
+		       		<td>
+		              		<div align="left">
+		          			<span class="content_1">Profile:</span>
+		                	</div>
+	        		</td>
+	        </tr>	
+		
+		
+		<tr>
+       	             <td>
+               	       	<div align="left">
+                			<span class="content_2">MR. <b>Zhou Junyang</b> has been working on his PhD in Department of Computer Science at Hong Kong Baptist University</span>                       				
+                       			<span class="content_2">after he received his <b>M.Sc.</b> in <b>Probability and Mathematic Statistics</b> from Sun Yat-Sen University, Guangzhou, China, 2003.</span>
+                       		        <span class="content_2">He also received his <b>B.Sc.</b> in <B>Applied Mathematics</B> from Department of Mathematic, Sun Yat-Sen University, Guangzhou, China, 2000. </span>
+             		</div>
+        	     </td>
+		</tr>
+
+	 	<tr><td height="10"></td></tr>
+		<tr>
+        	            <td>
+                	       	<div align="left">
+                       			<span class="content_2">His general research interests are wireless location, mobile/pervasive computing, Real-Time network and Bayesian Network. His supervisor is</span>
+                       				<a href="http://www.comp.hkbu.edu.hk/~jng" target=_blank><span class="url_1">Prof. Joseph Kee-Yin NG,</span></a>
+                       			<span class="content_2">and</span>
+                       				<a href="http://www.comp.hkbu.edu.hk/~xujl" target=_blank><span class="url_1">Dr. Jianliang Xu</span></a>
+                       			<span class="content_2">
+                       				is his co-supervisor. Mr. <B>Zhou</B> is good at modeling and algorithm analysis. 
+                       				Now he is working on the research about mobile station location estimation system (MLES).
+                       			</span>
+                       			
+                       		</div>
+                	     </td>
+		</tr>
+
+		</table>
+-->
+
+			
+			
+		
+		<div align="center"><hr noshade size=2 width="800" color=cccccc></div>		      
+
+
+		
+	
+
+	<!--counter-->	
+	
+	<P align=center>
+		<span class="content_2">Welcome to visit Dr. Zhou Junyang's homepage
+		</span>
+	</P>
+			
+	<P align=center>
+		<span class="content_2">You are the</span>
+		<span class="url_1">
+	<?php			
+	$counterFile = "/u/pg/jyzhou/public_html/counter/count.txt";  
+	$infofile = "/u/pg/jyzhou/public_html/counter/info.dat"; 
+	/*<!---counter--->*/
+	$fpr   = fopen($counterFile,"r");
+	$num  = fgets($fpr,10);
+	print "$num\n";
+	fclose($fpr);
+
+        $fpw = fopen($counterFile,"w");
+        $newnum = fputs($fpw, $num+1);
+  	fclose($fpw);
+
+	/*<!--writeinformation-->*/
+
+	 $fp = fopen($infofile,"a");
+	 $ip = $_SERVER['REMOTE_ADDR'];
+	 $hostname = gethostbyaddr($ip);
+	 $today = getdate();	 
+	 $day = $today[mday];
+	 $month = $today[month];
+	 $year = $today[year];
+	 $hour = $today[hours];
+	 $minute = $today[minutes];
+	 $second = $today[seconds];
+	 $weekday = $today[weekday];
+		 
+	 $dayinfo = $day . " , " . $month . " ," . $year. " ;" ;
+	 $timeinfo = $hour . ":" .$minute . ":" .$second. "." ;
+	 $date = $dayinfo."\t".$timeinfo; 
+	 $info = "\nHello, you are the " . $num . " visitor". ". Your ip is:".$ip.", Hostname:".$hostname.". \nThe date is :\t" . $date. " It'is :" . $weekday.".\n";
+	 $info = fputs($fp,$info);	
+	 fclose($fp);
+	?></span><span class="content_2">Vistor to visit this page. </span>
+	</P>
+	<p align=center><span class="content_2">Last updated on 15th December, 2006 </span></P>
+			
+	
+	</CENTER>
+	
+
+</BODY>
+
+</HTML>
+
+
