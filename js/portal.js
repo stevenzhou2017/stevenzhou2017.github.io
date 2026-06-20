df9884e591a2486e81fb083f30f0ed2b6bb366c8
Zhou Junyang <junyang.zhou@gmail.com>
2026-06-20 23:11:31 +0800

    Update portal.js

--- js/portal.js
+++ js/portal.js
@@ -1,65 +1,31 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
-// Smooth Scroll
+    // Smooth Scroll
+    document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+        anchor.addEventListener("click",function(e){
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")) ?.scrollIntoView({ behavior:"smooth"});
+        });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+     });
 
-anchor.addEventListener("click",function(e){
+    // Highlight Current Menu
+    const path=location.pathname;
+    document.querySelectorAll("nav a").forEach(a=>{
+        if(a.getAttribute("href")==path){
+            a.classList.add("active");
+        }
+    });
 
-e.preventDefault();
+    // Back To Top
+    const btn=document.getElementById("backTop");
+    if(btn){
+        window.addEventListener("scroll",()=>{
+            btn.style.display = window.scrollY>500?"block":"none";
+        });
 
-document.querySelector(this.getAttribute("href"))
-
-?.scrollIntoView({
-
-behavior:"smooth"
-
-});
-
-});
-
-});
-
-// Highlight Current Menu
-
-const path=location.pathname;
-
-document.querySelectorAll("nav a").forEach(a=>{
-
-if(a.getAttribute("href")==path){
-
-a.classList.add("active");
-
-}
-
-});
-
-// Back To Top
-
-const btn=document.getElementById("backTop");
-
-if(btn){
-
-window.addEventListener("scroll",()=>{
-
-btn.style.display=
-
-window.scrollY>500?"block":"none";
-
-});
-
-btn.onclick=()=>{
-
-window.scrollTo({
-
-top:0,
-
-behavior:"smooth"
-
-});
-
-};
-
-}
-
+        btn.onclick=()=>{
+            window.scrollTo({top:0, behavior:"smooth"});
+        };
+    }
 });
